901432c7cc4d6febb43466dedf3fc974f974794f
vincentyo2112 <YoTS.CE190435@gmail.com>
2026-03-19 14:50:14 +0700

    refacrtor ui

--- src/db/index.ts
+++ src/db/index.ts
@@ -2,7 +2,6 @@ import { drizzle } from 'drizzle-orm/postgres-js';
 import postgres from 'postgres';
 import * as dotenv from 'dotenv';
 
-// Nạp biến môi trường từ file .env
 dotenv.config();
 
 const connectionString = process.env.DATABASE_URL;
